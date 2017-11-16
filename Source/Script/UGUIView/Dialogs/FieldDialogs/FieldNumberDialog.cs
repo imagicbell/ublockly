d70d014cb8e61abc75aaef3853b4dba6d95914f5
maoling <maoling@putao.com>
2017-11-16 18:48:25 +0800

    1.0.0 UI：popup settings dialog for field

--- Source/Script/UGUIView/Dialogs/FieldDialogs/FieldNumberDialog.cs
+++ Source/Script/UGUIView/Dialogs/FieldDialogs/FieldNumberDialog.cs
@@ -1,7 +1,14 @@
-﻿namespace UBlockly.UGUI
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UBlockly.UGUI
 {
     public class FieldNumberDialog : BaseDialog
     {
+        [SerializeField] private Text m_LabelNum;
+        [SerializeField] private Button m_BtnClear;
+        [SerializeField] private LayoutGroup m_Group;
+        
         
     }
 }
