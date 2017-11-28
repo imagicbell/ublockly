b60a37bc5d2f5e3431bfd9b339309f282928f279
maoling <maoling@putao.com>
2017-11-28 19:11:56 +0800

    modify sync shell

--- sync-ptblockly-ublockly.sh
+++ sync-ptblockly-ublockly.sh
@@ -13,6 +13,13 @@ rm "$SOURCE_PATH"/Script/Test/CodeTest.meta
 rm -rf "$SOURCE_PATH"/Script/Test/ViewTest
 rm "$SOURCE_PATH"/Script/Test/ViewTest.meta
 
+#sync data
+SOURCE_DATA_PATH="/Users/$USER/Desktop/MSpace/UBlockly/UserData"
+TARGET_DATA_PATH="/Users/$USER/Desktop/PTProjects/Blockly/PTBlockly/Blockly/Assets/PTGameData/PTBlockly"
+
+rm -rf "$SOURCE_DATA_PATH"/I18n/*
+cp -a "$TARGET_DATA_PATH"/I18n/. "$SOURCE_DATA_PATH"/I18n
+
 cd "$SOURCE_PATH"
 find . -type f -name "*.cs" | while read filename; do
     sed -i '' 's/PTGame\.Blockly/UBlockly/g' "$filename"
